1efd04443dde681b65e613e628f374f81a0dbbbb
Topher <67612927+tkohldesac@users.noreply.github.com>
2023-06-13 00:44:03 -0700

    Initializing

--- frontend/components/TopBar.jsx
+++ frontend/components/TopBar.jsx
@@ -26,23 +26,6 @@ const cardStyle = {
 
 export default function TopBar() {
 
-    const [anchorEl, setAnchorEl] = useState(null);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-
-
-    // const userLoggedIn = useContext(userContext);
-
-    const [open, setOpen] = useState(false);
-    const modalOpen = () => setOpen(true);
-    const modalClose = () => setOpen(false);
 
 
     return (
