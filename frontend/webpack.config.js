3dfccd57486b159e37fa1370efde36947e29417d
Abel McElroy <abel.h.mcelroy@gmail.com>
2023-08-29 15:06:52 -0400

    changes webpack config to use mode for NODE_ENV

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,54 +1,56 @@
 const { DefinePlugin } = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    filename: 'main.js', 
-    path: path.resolve(__dirname, 'dist'), 
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: ['babel-loader'],
-      },{
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-            {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'images/',
-                    publicPath: 'images/',
-                },
-            },
-        ],
-    }, {
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    }, 
-    ],
-  },
-  resolve: {
-    extensions: ['.jsx', '.js', '.css'],
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+module.exports = (_, argv) => {
+  return {
+    entry: './index.js',
+    output: {
+      filename: 'main.js', 
+      path: path.resolve(__dirname, 'dist'), 
+      publicPath: '/',
     },
-    historyApiFallback: {
-      rewrites: [
-        { from: /^\/$/, to: '/index.html' },
-        { from: /main\.js$/, to: '/main.js' },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: ['babel-loader'],
+        },{
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+              {
+                  loader: 'file-loader',
+                  options: {
+                      name: '[name].[ext]',
+                      outputPath: 'images/',
+                      publicPath: 'images/',
+                  },
+              },
+          ],
+      }, {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      }, 
       ],
     },
-    compress: false,
-    port: 9000,
+    resolve: {
+      extensions: ['.jsx', '.js', '.css'],
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+      },
+      historyApiFallback: {
+        rewrites: [
+          { from: /^\/$/, to: '/index.html' },
+          { from: /main\.js$/, to: '/main.js' },
+        ],
+      },
+      compress: false,
+      port: 9000,
 
-  },
-  plugins: [ new DefinePlugin({
-    "process.env.NODE_ENV" : JSON.stringify(process.env.NODE_ENV)
-  })]
+    },
+    plugins: [ new DefinePlugin({
+      "process.env.NODE_ENV" : JSON.stringify(argv.mode)
+    })]
+  }
 };
