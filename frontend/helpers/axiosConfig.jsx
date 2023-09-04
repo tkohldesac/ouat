06cb12f4fed7d95d1073f4ba3aa3f19ef2e2eb33
Topher Kohlstedt <topher@kohldesac.com>
2023-09-03 18:36:19 -0700

    redeploying

--- frontend/helpers/axiosConfig.jsx
+++ frontend/helpers/axiosConfig.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://ouata-backend.vercel.app/" : 'http://localhost:4444';
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://ouata-server.vercel.app/" : 'http://localhost:4444';
 axios.defaults.withCredentials = true;
 
 export default axios;
